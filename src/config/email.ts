94f6d0ae27264cc46199e44ff60ce01f86dd7e1b
Ram Krishna Yadav <itsramky234@gmail.com>
2026-05-28 18:28:22 +0545

    feat(auth): implement email sending for OTP verification using sendEmail function

--- src/config/email.ts
+++ src/config/email.ts
@@ -3,16 +3,15 @@ import env from "./env.js";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
-async function sendAuthEmail() {
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["rayramkrishna0206@gmail.com"],
-    subject: "Hello World",
-    html: "<strong>It works!</strong>",
-  });
-  if(error){
-    return console.error({error})
-  }
+export async function sendEmail({to,subject,html}:{to:string,subject:string,html:string}){
+    const {data,error}=await resend.emails.send({
+        from: "Saleient <noreply@yadavram.com.np>",
+        to:to,
+        subject:subject,
+        html:html
+    })
+    if(error){
+        console.log("Error while sending email : ",error)
+    }
+    console.log("Email Sent Successfully.")
 }
-
-sendAuthEmail()
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,23 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "../db/config.js";
 import { magicLink } from "better-auth/plugins";
+import { sendEmail } from "../config/email.js";
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-    }),
-    plugins:[
-        magicLink({
-            sendMagicLink:async({email,url,token})=>{
-                
-            }
-        })
-    ]
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailOTP: {
+    enabled: true,
+    expiresIn:300,
+    async sendOTP({ email, otp }: { email: string; otp: string }) {
+      await sendEmail({
+        to: email,
+        subject: "Your verification code",
+        html:  `
+          <h1>${otp}</h1>
+          <p>Expires in 5 minutes</p>
+        `,
+      });
+    },
+  },
 });
