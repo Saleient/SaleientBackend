6a6f5aea2e011ff65e680985627c2fba5d936595
Ram Krishna Yadav <itsramky234@gmail.com>
2026-05-31 14:37:54 +0545

    feat(auth): refactor email sending logic to use createError for error handling

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,9 +1,10 @@
 import { Resend } from "resend";
 import env from "./env.js";
+import { createError } from "../utils/AppError.js";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
-export async function sendEmail({to,subject,html}:{to:string,subject:string,html:string}){
+async function sendEmail({to,subject,html}:{to:string,subject:string,html:string}){
     const {data,error}=await resend.emails.send({
         from: "Saleient <noreply@yadavram.com.np>",
         to:to,
@@ -12,6 +13,23 @@ export async function sendEmail({to,subject,html}:{to:string,subject:string,html
     })
     if(error){
         console.log("Error while sending email : ",error)
+        throw createError.badRequest("Error while sending verification mail.")
     }
     console.log("Email Sent Successfully.")
+}
+
+export async function sendVerificationOTP({ email, otp }: { email: string; otp: string }) {
+  try {
+    await sendEmail({
+      to: email,
+      subject: "Your verification code",
+      html: `
+        <h1>${otp}</h1>
+        <p>Expires in 5 minutes</p>
+      `,
+    });
+  } catch (err) {
+    console.error("Email send failed:", err);
+    throw createError.internal("Verification email could not be sent");
+  }
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,23 +2,26 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "../db/config.js";
 import { emailOTP } from "better-auth/plugins";
-import { sendEmail } from "../config/email.js";
-
+import { sendVerificationOTP } from "../config/email.js";
+import * as schema from "../db/schema.js";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
   plugins: [
     emailOTP({
       expiresIn: 300,
-      async sendVerificationOTP({ email, otp }: { email: string; otp: string }) {
-        await sendEmail({
-          to: email,
-          subject: "Your verification code",
-          html: `
-            <h1>${otp}</h1>
-            <p>Expires in 5 minutes</p>
-          `,
+      async sendVerificationOTP({
+        email,
+        otp,
+      }: {
+        email: string;
+        otp: string;
+      }) {
+        await sendVerificationOTP({
+          email: email,
+          otp,
         });
       },
     }),
