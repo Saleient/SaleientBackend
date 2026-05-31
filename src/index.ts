8f46fc1b73df87add10c61ec66f9bcdbde5cf480
Ram Krishna Yadav <itsramky234@gmail.com>
2026-05-31 11:09:39 +0545

    feat(auth): remove old authentication controller and service, add cors dependency

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from "express";
 import env from "./config/env.js";
 import type{ Request,Response } from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import authRouter from "./routes/auth.route.js";
-
+import { auth } from "./lib/auth.js";
+import cors from "cors"
+import { toNodeHandler } from "better-auth/node";
 const app=express();
 
 app.use(express.json())
-
-app.use("/api/auth",authRouter)
+app.use(cors({
+  origin:["http://localhost:5173"],
+  methods:["GET",'POST',"PUT","DELETE","PATCH"],
+  credentials:true
+}))
+app.all("/api/auth/*path",toNodeHandler(auth))
 
 app.get("/",async(req:Request,res:Response)=>{
     res.send("Saleient Backend Server is running")
