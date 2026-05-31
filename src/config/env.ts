452cb913cc5fb1060447fbb565a204f95ef89aa9
Ram Krishna Yadav <itsramky234@gmail.com>
2026-05-31 15:18:14 +0545

    feat(auth): add Google OAuth configuration using environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,6 +7,8 @@ const env={
     BETTER_AUTH_URL:process.env.BETTER_AUTH_URL,
     DATABASE_URL:process.env.DATABASE_URL,
     RESEND_API_KEY:process.env.RESEND_API_KEY,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID!,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET!,
     NODE_ENV:process.env.NODE_ENV || "development",
 }
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,21 @@ import db from "../db/config.js";
 import { emailOTP } from "better-auth/plugins";
 import { sendVerificationOTP } from "../config/email.js";
 import * as schema from "../db/schema.js";
+import env from "../config/env.js";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
   }),
+  trustedOrigins:[
+    "http://localhost:5173"
+  ],
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
   plugins: [
     emailOTP({
       expiresIn: 300,
